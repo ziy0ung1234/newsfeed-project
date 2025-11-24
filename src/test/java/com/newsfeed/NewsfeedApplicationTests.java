bcaa83355492cf86dc245059ae2185c974db21d1
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-11-24 10:43:52 +0900

    Initial commit

---
+++ src/test/java/com/newsfeed/NewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
