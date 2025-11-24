cf925e17fde77c2871c21fd0e64215089a852a0d
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-11-24 19:22:06 +0900

    feat: 유저 엔티티에 BaseDateEntity 상속 추가

--- src/main/java/com/newsfeed/common/entity/User.java
+++ src/main/java/com/newsfeed/common/entity/User.java
@@ -7,7 +7,7 @@ import lombok.*;
 @Entity
 @Table(name="users")
 @NoArgsConstructor(access= AccessLevel.PROTECTED)
-public class User {
+public class User extends BaseDateEntity {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
