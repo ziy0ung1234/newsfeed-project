9578a86eee44e8b9db62946850d393bf0d6bc7d7
sangmu <dltkdandl@naver.com>
2025-11-25 17:30:32 +0900

    ✨ feat : findByEmail jpa 생성

--- src/main/java/com/newsfeed/domain/user/repository/UserRepository.java
+++ src/main/java/com/newsfeed/domain/user/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.newsfeed.domain.user.repository;
 import com.newsfeed.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByCellPhoneNumber(String phone);
+    Optional<User> findByEmail(String email);
 }
