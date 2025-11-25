5145e4398a8d8821c3cf77073b30e63490af3fe2
aram0117 <dkfka568355@gmail.com>
2025-11-25 15:51:16 +0900

    feat: 유저본인 뉴스피드 조회 기능 추가

--- src/main/java/com/newsfeed/domain/newsfeed/service/NewsfeedService.java
+++ src/main/java/com/newsfeed/domain/newsfeed/service/NewsfeedService.java
@@ -10,6 +10,10 @@ import com.newsfeed.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.newsfeed.common.exception.ErrorCode.USER_NOT_FOUND;
 
 
@@ -23,11 +27,12 @@ public class NewsfeedService {
     @Transactional
     public NewsfeedResponse saveNewsfeed(NewsfeedRequest request, Long userId) {
 
+        // 유저아이디 검증
         User user = userRepository.findById(userId).orElseThrow(
                 () -> new NotFoundException(USER_NOT_FOUND)
         );
 
-        // 유저토큰 뉴스피드 제목, 내용 요청
+        // 유저아이디, 뉴스피드 제목과 내용 요청
         Newsfeed newsfeed = new Newsfeed(
                 request.getTitle(),
                 request.getContent(),
@@ -45,5 +50,32 @@ public class NewsfeedService {
                 saveNewsfeed.getCreatedAt(),
                 saveNewsfeed.getUpdatedAt()
         );
+    }
+
+    @Transactional
+    public List<NewsfeedResponse> myNewsfeed(Long userId) {
+
+        // 유저아이디 검증
+        userRepository.findById(userId).orElseThrow(
+                () -> new NotFoundException(USER_NOT_FOUND)
+        );
+        // 검증 된 유저아이디의 전체 뉴스피드
+        List<Newsfeed> myNewsfeeds = newsfeedRepository.findAll();
+
+        List<NewsfeedResponse> myNewsfeedList = new ArrayList<>();
+
+        // 순차 접근 후 뉴스피드 응답
+        for(Newsfeed newsfeed : myNewsfeeds) {
+            NewsfeedResponse newsfeedResponse = new NewsfeedResponse(
+                    newsfeed.getId(),
+                    newsfeed.getTitle(),
+                    newsfeed.getContent(),
+                    newsfeed.getCreatedAt(),
+                    newsfeed.getUpdatedAt()
+            );
+            // 리스트에 응답한 뉴스피드 객체들을 저장
+            myNewsfeedList.add(newsfeedResponse);
+        }
+        return myNewsfeedList;
     }
 }
