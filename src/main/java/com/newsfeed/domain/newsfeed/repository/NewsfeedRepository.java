11b0ef832e46687b6d9977b228f62f672b6a182a
aram0117 <dkfka568355@gmail.com>
2025-11-25 16:25:13 +0900

    refactor: 조회 메서드 findByUserId로 해당 유저의 뉴스피드 목록 조회를 수정

--- src/main/java/com/newsfeed/domain/newsfeed/repository/NewsfeedRepository.java
+++ src/main/java/com/newsfeed/domain/newsfeed/repository/NewsfeedRepository.java
@@ -3,5 +3,8 @@ package com.newsfeed.domain.newsfeed.repository;
 import com.newsfeed.domain.newsfeed.entity.Newsfeed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NewsfeedRepository extends JpaRepository<Newsfeed, Long> {
+    List<Newsfeed> findByUserId(Long userId);
 }
