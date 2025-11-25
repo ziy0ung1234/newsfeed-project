fea75b9814d526fe79194f840efca8fd32a57c32
jyop1212hy <jyop1212@icloud.com>
2025-11-25 09:40:38 +0900

    feat(User) : Add 프로필 조회/정보수정/회원탈퇴 - 컨트롤러 임시 구현]
    1. 각 메서드별 JWT 토큰 인증
    
    **인증/인가 로직 구현시 수정 예정**

---
+++ src/main/java/com/newsfeed/domain/user/controller/UserController.java
@@ -0,0 +1,72 @@
+package com.newsfeed.domain.user.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+    private final JwtProvider jwtProvider;
+
+    //프로필 조회
+    @GetMapping("/me")
+    public ResponseEntity<MyInfoResponse> myInfo(HttpServletRequest request) {
+
+        // 1. Authorization 헤더 체크
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("로그인이 유효 하지 않습니다.");
+        }
+
+        // 2. 토큰 추출
+        String token = authHeader.replace("Bearer ", "");
+
+        // 3. 토큰에서 userId 추출
+        Long userId = jwtProvider.getUserIdFromToken(token);
+
+        // 4. 서비스 호출
+        return ResponseEntity.ok(userService.myInfo(userId));
+    }
+
+    //정보 수정
+    @PutMapping("/me")
+    public ResponseEntity<UpdateResponse> update(HttpServletRequest tokenRequest, @Valid @RequestBody UpdateRequest upRequest) {
+        // 1. Authorization 헤더 체크
+        String authHeader = tokenRequest.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("로그인이 유효 하지 않습니다.");
+        }
+
+        // 2. 토큰 추출
+        String token = authHeader.replace("Bearer", "");
+
+        // 3. 토큰에서 userId 추출
+        Long userId = jwtProvider.getUserIdFromToken(token);
+
+        return ResponseEntity.ok(userService.update(userId,upRequest));
+    }
+
+
+    //회원탈퇴
+    public ResponseEntity<DeleteResponse> delete(HttpServletRequest tokenRequest) {
+        // 1. Authorization 헤더 체크
+        String authHeader = tokenRequest.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("로그인이 유효 하지 않습니다.");
+        }
+
+        // 2. 토큰 추출
+        String token = authHeader.replace("Bearer", "");
+
+        // 3. 토큰에서 userId 추출
+        Long userId = jwtProvider.getUserIdFromToken(token);
+
+        return ResponseEntity.ok(userId);
+    }
+}
