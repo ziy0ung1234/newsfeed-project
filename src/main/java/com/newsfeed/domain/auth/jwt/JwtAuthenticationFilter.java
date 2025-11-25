39cf133591e28055e619f5c4899ab2451c8fd257
sangmu <dltkdandl@naver.com>
2025-11-25 17:35:45 +0900

    🎉 add : jwt, spring security 생성. 필터 작업 완료

---
+++ src/main/java/com/newsfeed/domain/auth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,86 @@
+package com.newsfeed.domain.auth.jwt;
+
+import com.newsfeed.domain.auth.security.PrincipalDetails;
+import com.newsfeed.domain.auth.security.PrincipalDetailsService;
+import com.newsfeed.domain.user.entity.User;
+import com.newsfeed.domain.user.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+    private final PrincipalDetailsService principalDetailsService;
+
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+
+        String path = request.getServletPath();
+
+        // 인증이 필요없는 경로는 필터 제외
+        return path.equals("/user/signup")
+                || path.equals("/user/login");
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+        throws ServletException, IOException {
+
+        String token = resolveToken(request);
+
+        // 토큰이 유효 했을 때
+        if (token != null && jwtProvider.validateToken(token)) {
+
+            Long userId = jwtProvider.getUserId(token);
+            PrincipalDetails principalDetails = (PrincipalDetails) principalDetailsService.loadUserByUsername(String.valueOf(userId));
+//            User user = userRepository.findById(userId).orElse(null);
+
+//            if (user != null) {
+//                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, null);
+//
+//                SecurityContextHolder.getContext().setAuthentication(authentication);
+//            }
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            principalDetails,
+                            null,
+                            principalDetails.getAuthorities()
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        // 토큰이 유효하지 않을 때 처리(후순위)
+        System.out.println("JWT token: " + token);
+        System.out.println("Valid: " + jwtProvider.validateToken(token));
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader("Authorization");
+
+        if (bearer != null && bearer.startsWith("Bearer ")) {
+            return bearer.substring(7);
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/com/newsfeed/domain/auth/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.newsfeed.domain.auth.security;
+
+import com.newsfeed.domain.auth.jwt.JwtAuthenticationEntryPoint;
+import com.newsfeed.domain.auth.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
