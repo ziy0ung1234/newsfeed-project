c6f87cf65a13864f842324b2d2e2885d920bfd08
sangmu <dltkdandl@naver.com>
2025-11-26 14:49:23 +0900

    ✨ feat	: dto에서 유효성 검증 코드 구현

--- src/main/java/com/newsfeed/domain/auth/dto/LoginRequest.java
+++ src/main/java/com/newsfeed/domain/auth/dto/LoginRequest.java
@@ -1,9 +1,16 @@
 package com.newsfeed.domain.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class LoginRequest {
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Size(max = 30, message = "이메일은 30자 이하로 입력해주세요.")
     private String email;
+
+    @NotBlank(message = "비밀번호은 필수 입력 값입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
     private String password;
 }
