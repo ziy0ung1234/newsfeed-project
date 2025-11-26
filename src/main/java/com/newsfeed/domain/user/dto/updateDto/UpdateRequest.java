a3eac0bd2c6bb7cfd82f3bee73824373df92e9f2
jyop1212hy <jyop1212@icloud.com>
2025-11-26 13:32:14 +0900

    refactor : UpdateRequest 내부 필드에 검증용 어노테이션 추가

--- src/main/java/com/newsfeed/domain/user/dto/updateDto/UpdateRequest.java
+++ src/main/java/com/newsfeed/domain/user/dto/updateDto/UpdateRequest.java
@@ -1,6 +1,6 @@
 package com.newsfeed.domain.user.dto.updateDto;
 
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
@@ -8,7 +8,12 @@ public class UpdateRequest {
 
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    @Size(min = 1, max = 20, message = "이름은 1~20자 사이여야 합니다.")
     private String username;
+
+    @Size(min = 11, max = 11, message = "휴대폰 번호는 010 포함 11자리여야 합니다.")
+    @PositiveOrZero
     private String cellphone;
 
     public UpdateRequest(String email, String username, String cellphone) {
