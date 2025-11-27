e28636a2e29bde4b4d124fa1a61d76bc6a93fd46
jyop1212hy <jyop1212@icloud.com>
2025-11-27 15:27:49 +0900

    add : FollowersResponse 생성 add : FollowCountResponse 생성 add : FollowUserResponse 생성
    feat : 팔로잉,팔로워 유저목록 리스트 조회 API 추가
    
    feat : FollowRepository
    조회 쿼리문 추가
    countByFollowerId(나 -> 타인)
    countByFollowingId(나 <- 타인)
    findAllByFollowerId(내가 팔로우한 사람들 목록)
    findAllByFollowingId(나를 팔로우한 사람들 목록)

--- src/main/java/com/newsfeed/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/newsfeed/domain/follow/repository/FollowRepository.java
@@ -3,8 +3,22 @@ package com.newsfeed.domain.follow.repository;
 import com.newsfeed.domain.follow.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     Optional<Follow> findByFollowerIdAndFollowingId(Long userId, Long otherId);
+
+    // 내가 팔로우한 사람 수 (나 → 다른 사람들)
+    Long countByFollowerId(Long userId);
+
+    // 나를 팔로우한 사람 수 (다른 사람들 → 나)
+    Long countByFollowingId(Long userId);
+
+
+    // 내가 팔로우한 사람들 목록
+    List<Follow> findAllByFollowerId(Long userId);
+
+    // 나를 팔로우한 사람들 목록
+    List<Follow> findAllByFollowingId(Long userId);
 }
