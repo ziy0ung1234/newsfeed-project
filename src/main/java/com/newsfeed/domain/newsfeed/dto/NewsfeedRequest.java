0c1057f8d065c285bfecf3ca8d006948cadd7c98
aram0117 <dkfka568355@gmail.com>
2025-11-27 19:25:11 +0900

    refactor: 벨리데이션 메시지 수정

--- src/main/java/com/newsfeed/domain/newsfeed/dto/NewsfeedRequest.java
+++ src/main/java/com/newsfeed/domain/newsfeed/dto/NewsfeedRequest.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 public class NewsfeedRequest {
 
     @NotBlank(message = "제목을 입력해 주세요")
-    @Size(max = 15)
+    @Size(max = 15, message = "제목은 15자 이내로 입력해주세요")
     private String title;
-    @Size(max = 500)
     @NotBlank(message = "내용을 입력해 주세요")
+    @Size(max = 500, message =  "내용은 500자 이내로 입력해주세요")
     private String content;
 }
